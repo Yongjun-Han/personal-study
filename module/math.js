bda6523a92a6e91494c2baaf5d3b02b5fa469d3c
hanyongjun <hanyongjun.dev@gmail.com>
2022-08-30 23:59:03 +0900

    모듈학습

---
+++ module/math.js
@@ -0,0 +1,12 @@
+const perfectScore = 100;
+
+const sum = (num1, num2) => {
+  return num1+num2;
+}
+
+// export const avg = (num1, num2) => {
+//   return (num1+num2) / 2;
+// }
+
+export default sum;
+
---
+++ module/module.js
@@ -0,0 +1,8 @@
+// 모듈 : 코드의 재활용성을 높이고 유지보수를 쉽게 할 수 있게 코드를 부품화 하는것 
+//기존의 모듈방식은 전역스코프기 동시에 사용되서 문제가 있었다. 
+
+// <script src "./ex.js", type = "module"></script> --> 이 파일은 모듈로 동작한다 
+// export default function name() {}
+// import name form "ex.js" 
+
+
