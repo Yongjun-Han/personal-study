1bbd6411fe3cac522b7ee949665334c7921a0972
hanyongjun <hanyongjun.dev@gmail.com>
2022-09-01 00:07:36 +0900

    생성자함수

--- 생성자함수/constructor_func.js
+++ 생성자함수/constructor_func.js
@@ -8,11 +8,15 @@ function User(name,age) {
   this.age = age;
   // 2.함수를 실행하며 this에 porperty들을 추가한다
   // 3. return this; 
-  
+  this.sayName = () => {console.log(this.name)}
+  //                          여기서의 this 는 user2
 }
 
 const user1 = new User("jason", 23);
+const user2 = new User("han", 12);
 
 console.log(user1);
+user2.sayName();
+
 
 // 객체리터럴보다 훨씬 편리하고 빠르게 객체를 안정적으로 생성할 수 있으며 유지보수 하기에 좋다 
